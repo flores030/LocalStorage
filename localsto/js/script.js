0472181ce4175f0e5d65ee77a80b04ace70f9a76
flores030 <99411537+flores030@users.noreply.github.com>
2022-06-02 12:50:13 -0300

    Add files via upload

---
+++ localsto/js/script.js
@@ -0,0 +1,33 @@
+let tarefas = []
+
+const addTarefa = () => {
+    let tarefa = document.getElementById('tarefa').value
+    document.getElementById('tarefa').value= ""
+
+    if(localStorage.getItem('tarefas'))
+    tarefas = JSON.parse( localStorage.getItem('tarefas'))
+
+let data = new Date();
+let date = `${data.getUTCDay()}/${data.getUTCMonth()}/${data.getUTCFullYear()}`;
+
+tarefas.push([date, tarefa])
+localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
+const list = () => {
+    let tbody = document.querySelector('#tr-tarefa')
+    if (localStorage.getItem('tarefas')!=null){
+        tarefas= JSON.parse(localStorage.getItem('tarefas'))
+    } else {
+        tarefas.push (["Sem Dados "," Sem Dados"])
+    }
+     
+    
+    tarefas.forEach(element => {
+        tbody.innerHTML = "<tr> <td>"+ element[0] +" </td> <td>" + element[1]+"</td> </tr>"
+        
+    });
+
+        
+
+}
